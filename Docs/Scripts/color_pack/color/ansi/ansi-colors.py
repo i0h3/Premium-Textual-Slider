aa72bc0577e29b10dfe10850053d6ae7fae8671b
Will <274450921+i0h3@users.noreply.github.com>
2026-04-19 00:41:43 -0500

    ansi-colors.py

---
+++ Docs/Scripts/color_pack/color/ansi/ansi-colors.py
@@ -0,0 +1,19 @@
+from textual.color import Color
+
+ansi_black         = Color(0, 0, 0)
+ansi_red           = Color(205, 0, 0)
+ansi_green         = Color(0, 205, 0)
+ansi_yellow        = Color(205, 205, 0)
+ansi_blue          = Color(0, 0, 238)
+ansi_magenta       = Color(205, 0, 205)
+ansi_cyan          = Color(0, 205, 205)
+ansi_white         = Color(229, 229, 229)
+
+ansi_bright_black   = Color(127, 127, 127)
+ansi_bright_red     = Color(255, 0, 0)
+ansi_bright_green   = Color(0, 255, 0)
+ansi_bright_yellow  = Color(255, 255, 0)
+ansi_bright_blue    = Color(92, 92, 255)
+ansi_bright_magenta = Color(255, 0, 255)
+ansi_bright_cyan    = Color(0, 255, 255)
+ansi_bright_white   = Color(255, 255, 255)
